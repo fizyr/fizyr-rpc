21dcc0e75db788bc8d7be607eae361e6ea66b8cc
Maarten de Vries <maarten@de-vri.es>
2020-09-22 21:12:16 +0200

    Allow closing a connection through a peer handle.

--- src/peer.rs
+++ src/peer.rs
@@ -8,22 +8,28 @@ use crate::error;
 
 /// Handle to a peer.
 pub struct PeerHandle<Body> {
-	read_half: PeerReadHandle<Body>,
-	write_half: PeerWriteHandle<Body>,
+	read_handle: PeerReadHandle<Body>,
+	write_handle: PeerWriteHandle<Body>,
 }
 
-/// The read half of a peer.
+/// Handle to receive messages from a peer.
 ///
-/// The read half can be used to receive incoming requests and stream messages.
+/// The read handle can be used to receive incoming requests and stream messages.
 pub struct PeerReadHandle<Body> {
 	incoming_rx: mpsc::UnboundedReceiver<Result<Incoming<Body>, error::NextMessageError>>,
 	command_tx: mpsc::UnboundedSender<Command<Body>>,
 }
 
-/// The write half of a peer.
+/// Handle to send messages to a peer.
 ///
-/// The write half can be used to send request and stream messages.
+/// The write handle can be used to send requests and stream messages.
 pub struct PeerWriteHandle<Body> {
+	command_tx: mpsc::UnboundedSender<Command<Body>>,
+}
+
+/// Handle to close the connection with a peer.
+#[derive(Clone)]
+pub struct PeerCloseHandle<Body> {
 	command_tx: mpsc::UnboundedSender<Command<Body>>,
 }
 
@@ -38,20 +44,21 @@ pub enum Command<Body> {
 }
 
 impl<Body> PeerHandle<Body> {
+	/// Create a new peer handle from the separate channels.
 	pub(crate) fn new(
 		incoming_rx: mpsc::UnboundedReceiver<Result<Incoming<Body>, error::NextMessageError>>,
 		command_tx: mpsc::UnboundedSender<Command<Body>>,
 	) -> Self {
-		let read_half = PeerReadHandle { incoming_rx, command_tx: command_tx.clone() };
-		let write_half = PeerWriteHandle { command_tx };
-		Self { read_half, write_half }
+		let read_handle = PeerReadHandle { incoming_rx, command_tx: command_tx.clone() };
+		let write_handle = PeerWriteHandle { command_tx };
+		Self { read_handle, write_handle }
 	}
 
-	/// Split the peer in a read half and a write half.
+	/// Split the peer in a read handle and a write handle.
 	///
-	/// Splitting the peer allows you to move both halfs into different tasks.
+	/// Splitting the peer allows you to move both handles into different tasks.
 	pub fn split(self) -> (PeerReadHandle<Body>, PeerWriteHandle<Body>) {
-		(self.read_half, self.write_half)
+		(self.read_handle, self.write_handle)
 	}
 
 	/// Get the next request or stream message from the remote peer.
@@ -59,17 +66,66 @@ impl<Body> PeerHandle<Body> {
 	/// Errors for invalid incoming messages are also reported by this function.
 	/// For example: incoming update messages that are not associated with a received request will be reported as an error here.
 	pub async fn next_message(&mut self) -> Result<Incoming<Body>, error::NextMessageError> {
-		self.read_half.next_message().await
+		self.read_handle.next_message().await
 	}
 
 	/// Send a new request to the remote peer.
 	pub async fn send_request(&mut self, service_id: i32, body: impl Into<Body>) -> Result<SentRequest<Body>, error::SendRequestError> {
-		self.write_half.send_request(service_id, body).await
+		self.write_handle.send_request(service_id, body).await
 	}
 
 	/// Send a stream message to the remote peer.
 	pub async fn send_stream(&mut self, service_id: i32, body: impl Into<Body>) -> Result<(), error::WriteMessageError> {
-		self.write_half.send_stream(service_id, body).await
+		self.write_handle.send_stream(service_id, body).await
+	}
+
+	/// Close the connection with the remote peer.
+	///
+	/// This is equivalent to just dropping the handle.
+	pub fn close(self) {
+		drop(self)
+	}
+
+	/// Make a close handle for the peer.
+	///
+	/// The close handle can be used to close the connection with the remote peer.
+	/// It can be cloned and moved around independently.
+	pub fn close_handle(&self) -> PeerCloseHandle<Body> {
+		self.read_handle.close_handle()
+	}
+}
+
+impl<Body> PeerReadHandle<Body> {
+	/// Get the next request or stream message from the remote peer.
+	///
+	/// Errors for invalid incoming messages are also reported by this function.
+	/// For example: incoming update messages that are not associated with a received request will be reported as an error here.
+	pub async fn next_message(&mut self) -> Result<Incoming<Body>, error::NextMessageError> {
+		self.incoming_rx.recv().await.ok_or_else(error::not_connected)?
+	}
+
+	/// Close the connection with the remote peer.
+	///
+	/// Dropping both the read and write handle will close the channel.
+	/// This function can close the channel even if the other handle is still around.
+	pub fn close(&self) {
+		let _ = self.command_tx.send(Command::Stop);
+	}
+
+	/// Make a close handle for the peer.
+	///
+	/// The close handle can be used to close the connection with the remote peer.
+	/// It can be cloned and moved around independently.
+	pub fn close_handle(&self) -> PeerCloseHandle<Body> {
+		PeerCloseHandle {
+			command_tx: self.command_tx.clone(),
+		}
+	}
+}
+
+impl<Body> Drop for PeerReadHandle<Body> {
+	fn drop(&mut self) {
+		let _ = self.command_tx.send(Command::StopReadHalf);
 	}
 }
 
@@ -94,6 +150,24 @@ impl<Body> PeerWriteHandle<Body> {
 
 		result_rx.await.map_err(|_| error::not_connected())?
 	}
+
+	/// Close the connection with the remote peer.
+	///
+	/// Dropping both the read and write handle will close the channel.
+	/// This function can close the channel even if the other handle is still around.
+	pub fn close(&self) {
+		let _ = self.command_tx.send(Command::Stop);
+	}
+
+	/// Make a close handle for the peer.
+	///
+	/// The close handle can be used to close the connection with the remote peer.
+	/// It can be cloned and moved around independently.
+	pub fn close_handle(&self) -> PeerCloseHandle<Body> {
+		PeerCloseHandle {
+			command_tx: self.command_tx.clone(),
+		}
+	}
 }
 
 impl<Body> Drop for PeerWriteHandle<Body> {
@@ -102,19 +176,10 @@ impl<Body> Drop for PeerWriteHandle<Body> {
 	}
 }
 
-impl<Body> PeerReadHandle<Body> {
-	/// Get the next request or stream message from the remote peer.
-	///
-	/// Errors for invalid incoming messages are also reported by this function.
-	/// For example: incoming update messages that are not associated with a received request will be reported as an error here.
-	pub async fn next_message(&mut self) -> Result<Incoming<Body>, error::NextMessageError> {
-		self.incoming_rx.recv().await.ok_or_else(error::not_connected)?
-	}
-}
-
-impl<Body> Drop for PeerReadHandle<Body> {
-	fn drop(&mut self) {
-		let _ = self.command_tx.send(Command::StopReadHalf);
+impl<Body> PeerCloseHandle<Body> {
+	/// Close the connection with the remote peer.
+	pub fn close(&self) {
+		let _ = self.command_tx.send(Command::Stop);
 	}
 }
 
