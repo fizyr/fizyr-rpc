b722515e4d9694305b748224a5490717410afa50
Maarten de Vries <maarten@de-vri.es>
2020-10-13 20:42:12 +0200

    Allow `ready` macro to be unused when all features are disabled.

--- src/macros.rs
+++ src/macros.rs
@@ -1,6 +1,7 @@
 /// Unwrap a [`Poll`](std::task::Poll) value and return from the enclosing function if it was [`Pending`](std::task::Poll::Pending).
 ///
 /// This is like `try!()`, but for [`Poll`](std::task::Poll).
+#[allow(unused_macros)]
 macro_rules! ready {
 	($e:expr) => {
 		match $e {
