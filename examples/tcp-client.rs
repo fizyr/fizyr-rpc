139a341596dad79dc607b0cbae89023d19a47826
Maarten de Vries <maarten@de-vri.es>
2021-04-25 16:55:30 +0200

    Add TCP examples.

---
+++ examples/tcp-client.rs
@@ -0,0 +1,54 @@
+use fizyr_rpc::TcpPeer;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(setting = structopt::clap::AppSettings::ColoredHelp)]
+#[structopt(setting = structopt::clap::AppSettings::UnifiedHelpMessage)]
+#[structopt(setting = structopt::clap::AppSettings::DeriveDisplayOrder)]
+struct Options {
+	#[structopt(default_value = "localhost:12345")]
+	address: String,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+	if let Err(e) = do_main(&Options::from_args()).await {
+		eprintln!("Error: {}", e);
+		std::process::exit(1);
+	}
+}
+
+async fn do_main(options: &Options) -> Result<(), String> {
+	// Connect to a remote server.
+	let mut peer = TcpPeer::connect(&options.address, Default::default()).await
+		.map_err(|e| format!("failed to connect to {}: {}", options.address, e))?;
+
+	// Send a request to the remote peer.
+	let mut request = peer
+		.send_request(1, &b"Hello World!"[..])
+		.await
+		.map_err(|e| format!("failed to send request: {}", e))?;
+
+	loop {
+		let update = request
+			.recv_update()
+			.await
+			.map_err(|e| format!("failed to read message: {}", e))?;
+		let update = match update {
+			Some(x) => x,
+			None => break,
+		};
+		let message = std::str::from_utf8(&update.body.data).map_err(|_| "invalid UTF-8 in update")?;
+		eprintln!("Received update: {}", message);
+	}
+
+	let response = request
+		.recv_response()
+		.await
+		.map_err(|e| format!("failed to read message: {}", e))?;
+	// Parse the message body as UTF-8, print it and exit the loop.
+	let message = std::str::from_utf8(&response.body.data).map_err(|_| "invalid UTF-8 in response")?;
+	eprintln!("Received response: {}", message);
+
+	Ok(())
+}
---
+++ examples/tcp-server.rs
@@ -0,0 +1,87 @@
+use fizyr_rpc::TcpServer;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(setting = structopt::clap::AppSettings::ColoredHelp)]
+#[structopt(setting = structopt::clap::AppSettings::UnifiedHelpMessage)]
+#[structopt(setting = structopt::clap::AppSettings::DeriveDisplayOrder)]
+struct Options {
+	#[structopt(default_value = "[::]:12345")]
+	bind: String,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+	if let Err(e) = do_main(&Options::from_args()).await {
+		eprintln!("Error: {}", e);
+		std::process::exit(1);
+	}
+}
+
+async fn do_main(options: &Options) -> Result<(), String> {
+	// Create the server.
+	let mut server = TcpServer::bind(options.bind.as_str(), Default::default())
+		.await
+		.map_err(|e| format!("failed to bind to {}: {}", options.bind, e))?;
+	eprintln!("listening on {}", options.bind);
+
+	// Run the accept loop.
+	// The lambda returns a future that will be spawned in a new task for each peer.
+	let result = server.run(|peer| async {
+		if let Err(e) = handle_peer(peer).await {
+			eprintln!("Error: {}", e);
+		}
+	});
+
+	// Pass up errors from the accept loop.
+	result.await.map_err(|e| format!("error in accept loop: {}", e))?;
+
+	Ok(())
+}
+
+/// Handle communication with a single peer.
+async fn handle_peer(mut peer: fizyr_rpc::PeerHandle<fizyr_rpc::StreamBody>) -> Result<(), String> {
+	eprintln!("new connection accepted");
+	loop {
+		// Receive the next incoming message.
+		let incoming = match peer.next_message().await {
+			Ok(x) => x,
+			Err(e) => {
+				if e.is_connection_aborted() {
+					// Log aborted connections but return Ok(()).
+					eprintln!("connection closed by peer");
+					return Ok(());
+				} else {
+					// Pass other errors up to the caller.
+					return Err(format!("failed to receive message from peer: {}", e));
+				}
+			},
+		};
+
+		// Handle the incoming message.
+		match incoming {
+			fizyr_rpc::Incoming::Stream(msg) => eprintln!("unspported stream message received: {:?}", msg),
+			fizyr_rpc::Incoming::Request(request) => match request.service_id() {
+				1 => handle_hello(request).await?,
+				n => request
+					.send_error_response(&format!("unknown service ID: {}", n))
+					.await
+					.map_err(|e| format!("failed to send error response message: {}", e))?,
+			},
+		}
+	}
+}
+
+async fn handle_hello(request: fizyr_rpc::ReceivedRequest<fizyr_rpc::StreamBody>) -> Result<(), String> {
+	// Parse the request body as UTF-8 and print it.
+	let message = std::str::from_utf8(request.body()).map_err(|_| "invalid UTF-8 in hello message")?;
+	eprintln!("received hello request: {}", message);
+
+	// Send a goodbye response.
+	request
+		.send_response(1, &b"Goodbye!"[..])
+		.await
+		.map_err(|e| format!("failed to send goodbye response: {}", e))?;
+
+	Ok(())
+}
