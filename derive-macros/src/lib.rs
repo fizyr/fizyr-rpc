b88c1391baa362fcc067f7f93e3543a648a74e5f
Maarten de Vries <maarten@de-vri.es>
2021-05-09 17:01:00 +0200

    Implement argument parsing for new `interface!{}` macro.

---
+++ derive-macros/src/lib.rs
@@ -0,0 +1,8 @@
+mod interface;
+mod util;
+
+/// Define an RPC interface.
+#[proc_macro]
+pub fn interface(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
+	interface::generate_interface(tokens.into()).into()
+}
---
+++ derive-macros/src/util.rs
@@ -0,0 +1,64 @@
+use proc_macro2::{Span, TokenStream};
+use syn::parse::{Parse, ParseStream};
+
+/// A span and a value.
+///
+/// Mainly used to keep spans around for generating errors later on.
+#[derive(Debug)]
+pub struct WithSpan<T> {
+	pub span: Span,
+	pub value: T,
+}
+
+impl<T> WithSpan<T> {
+	/// Create a new `WithSpan` from a [`Span`] and a value.
+	pub fn new(span: Span, value: T) -> Self {
+		Self { span, value }
+	}
+}
+
+impl<T> syn::spanned::Spanned for WithSpan<T> {
+	fn span(&self) -> Span {
+		self.span
+	}
+}
+
+/// Keep parsing a type until the stream is depleted.
+///
+/// The type is parsed as-is without expecting any intermediate puncuation.
+pub fn parse_repeated<T: Parse>(input: ParseStream) -> syn::Result<Vec<T>> {
+	let mut result = Vec::new();
+	while !input.is_empty() {
+		result.push(input.parse()?);
+	}
+	Ok(result)
+}
+
+/// Helper struct to parse `= T` from a token stream.
+struct EqAttrContents<T> {
+	_eq_token: syn::token::Eq,
+	value: T,
+}
+
+impl<T: Parse> Parse for EqAttrContents<T> {
+	fn parse(input: ParseStream) -> syn::Result<Self> {
+		Ok(Self {
+			_eq_token: input.parse()?,
+			value: input.parse()?,
+		})
+	}
+}
+
+/// Parse the input tokens as `= T`.
+///
+/// This is useful for parsing `#[attr = value]` style attributes.
+pub fn parse_eq_attr_contents<T: Parse>(input: TokenStream) -> syn::Result<T> {
+	let parsed: EqAttrContents<T> = syn::parse2(input)?;
+	Ok(parsed.value)
+}
+
+/// Parse the string value of a doc attribute.
+pub fn parse_doc_attr_contents(input: TokenStream) -> syn::Result<WithSpan<String>> {
+	let doc: syn::LitStr = parse_eq_attr_contents(input)?;
+	Ok(WithSpan::new(doc.span(), doc.value()))
+}
